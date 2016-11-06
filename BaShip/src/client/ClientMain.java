d7093ba923abf0e54acbd29236a973c71780a09f
Alexandre Truppel <alex.truppel@gmail.com>
2016-11-06 21:24:50 +0000

    Modified Packet classes

--- BaShip/src/client/ClientMain.java
+++ BaShip/src/client/ClientMain.java
@@ -1,46 +1,41 @@
 package client;
 
+import client.ui.*;
 import java.io.IOException;
 import java.net.*;
 import sharedlib.coms.*;
 import sharedlib.config.*;
-import client.ui.*;
 
 public class ClientMain {
 
-    private ClientMain()
-    {
+    private ClientMain() {
         
     }
-    
-    public static final ClientMain instance = new ClientMain(); //singleton
-    
+
+    public static final ClientMain instance = new ClientMain(); // Singleton
     private static final MainFrame mainFrame = new MainFrame();
-    
+
     public static final Configuration config = new Configuration("src/client/config.properties");
     public static ServerConnection connection;
 
-    public static MainFrame getMainFrame()
-    {
+    public static MainFrame getMainFrame() {
         return mainFrame;
     }
-    
+
     public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
-        
+
         /*java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 mainFrame.setVisible(true);
             }
         });*/
-        
-        /*java.awt.EventQueue.invokeLater(new Runnable() {
+ /*java.awt.EventQueue.invokeLater(new Runnable() {
               public void run() {
                   loginPanel.setVisible(true);
               }
         });*/
-        
         // Create socket and connect
-        Socket socket = new Socket(config.getS("server.ip"), config.getI("server.port"));        
+        Socket socket = new Socket(config.getS("server.ip"), config.getI("server.port"));
         connection = new ServerConnection(socket);
         connection.start();
 
